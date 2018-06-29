e882894f049398ddb9cccbeb0ab78078d22336f2
labuser <labuser@eusadminss-iMac-20.local>
2018-06-29 16:46:44 -0500

    can saved drawings to photos library

--- MichaelChang-Lab3/MichaelChang-Lab3/ViewController.swift
+++ MichaelChang-Lab3/MichaelChang-Lab3/ViewController.swift
@@ -5,16 +5,56 @@
 //  Created by labuser on 6/28/18.
 //  Copyright © 2018 wustl. All rights reserved.
 //
+// Credits
+// 1. How to take a screenshot of a UIView (https://stackoverflow.com/questions/31582222/swift-take-sceenshot-of-a-uiview)
+// 2. How to save a UI Image to Photos library (https://www.hackingwithswift.com/example-code/media/uiimagewritetosavedphotosalbum-how-to-write-to-the-ios-photo-album)
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var canvasView: CanvasView!
-    @IBOutlet weak var lineWidthSlider: UISlider!    
+    @IBOutlet weak var canvasView: CanvasView!
+    @IBOutlet weak var lineWidthSlider: UISlider!
     
     var currPath : PathView?
     var currentColor = UIColor.black
+    
+    @IBAction func saveCanvas(_ sender: Any) {
+        let screenShot = takeScreenshot()
+        saveScreenshot(screenShot : screenShot)
+    }
+    
+    func takeScreenshot() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(canvasView.bounds.size, false, UIScreen.main.scale)
+        
+        canvasView.drawHierarchy(in: canvasView.bounds, afterScreenUpdates: true)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if (image != nil)
+        {
+            return image!
+        }
+        return UIImage()
+    }
+    
+    func saveScreenshot(screenShot : UIImage) {
+        UIImageWriteToSavedPhotosAlbum(screenShot, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
     
     @IBAction func colorPicked(_ sender: Any) {
         if let color = (sender as! UIButton).backgroundColor {
@@ -59,8 +99,6 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
+    }    
 }
 
